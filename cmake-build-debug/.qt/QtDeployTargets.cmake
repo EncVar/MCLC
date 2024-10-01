70cd89df8320daf8d84d27ede24ccbccd70ce7df
EncVar <gbc034@outlook.com>
2024-10-01 20:57:55 +0800

    initial commit

---
+++ cmake-build-debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_MCLC_FILE C:/Users/EncVar/CLionProjects/MCLC/cmake-build-debug/MCLC.exe)
+set(__QT_DEPLOY_TARGET_MCLC_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_MCLC_RUNTIME_DLLS D:/Qt/6.8.0/llvm-mingw_64/bin/Qt6Core.dll)
---
+++ src/MCLC.cpp
@@ -0,0 +1,7 @@
+#include <QCoreApplication>
+
+//MCLC Main
+int main(int argc, char *argv[]) {
+    QCoreApplication app(argc, argv);
+    return QCoreApplication::exec();
+}
